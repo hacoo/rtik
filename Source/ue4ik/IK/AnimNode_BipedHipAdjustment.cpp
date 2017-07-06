26b406266dd04236baf0e171283936833de4cf2e
Henry <hacoo36@gmail.com>
2017-07-06 16:46:55 -0700

    Added FAnimNode_SkeletalControlBase boilerplate to AnimNode_BipedHipAdjustment

--- Source/ue4ik/IK/AnimNode_BipedHipAdjustment.cpp
+++ Source/ue4ik/IK/AnimNode_BipedHipAdjustment.cpp
@@ -1,11 +1,45 @@
 // Copyright (c) Henry Cooney 2017
 
 #include "AnimNode_BipedHipAdjustment.h"
+#include "IK.h"
 
-AnimNode_BipedHipAdjustment::AnimNode_BipedHipAdjustment()
+void FAnimNode_BipedHipAdjustment::UpdateInternal(const FAnimationUpdateContext & Context)
+{
+	FAnimNode_SkeletalControlBase::UpdateInternal(Context);
+	DeltaTime = Context.GetDeltaTime();
+}
+
+void FAnimNode_BipedHipAdjustment::EvaluateComponentSpaceInternal(FComponentSpacePoseContext & Context)
 {
 }
 
-AnimNode_BipedHipAdjustment::~AnimNode_BipedHipAdjustment()
+void FAnimNode_BipedHipAdjustment::EvaluateSkeletalControl_AnyThread(FComponentSpacePoseContext & Output, TArray<FBoneTransform>& OutBoneTransforms)
 {
+}
+
+bool FAnimNode_BipedHipAdjustment::IsValidToEvaluate(const USkeleton * Skeleton, const FBoneContainer & RequiredBones)
+{
+	return bInitSuccess;
+}
+
+void FAnimNode_BipedHipAdjustment::InitializeBoneReferences(const FBoneContainer & RequiredBones)
+{
+	bInitSuccess = true;
+	if (!RightLeg.InitAndAssignBones(RequiredBones))
+	{
+		UE_LOG(LogIK, Warning, TEXT("Could not initialize right leg for biped hip adjustment"));
+		bInitSuccess = false;
+	}
+
+	if (!LeftLeg.InitAndAssignBones(RequiredBones))
+	{
+		UE_LOG(LogIK, Warning, TEXT("Could not initialize left leg for biped hip adjustment"));
+		bInitSuccess = false;
+	}
+
+	if (!PelvisBone.Init(RequiredBones))
+	{
+		UE_LOG(LogIK, Warning, TEXT("Could not initialize pelvis bone for biped hip adjustment"));
+		bInitSuccess = false;
+	}
 }
--- Source/ue4ik/IK/AnimNode_BipedHipAdjustment.h
+++ Source/ue4ik/IK/AnimNode_BipedHipAdjustment.h
@@ -3,13 +3,50 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "IK.h"
+#include "BoneControllers/AnimNode_SkeletalControlBase.h"
+#include "AnimNode_BipedHipAdjustment.generated.h"
 
 /**
  * 
  */
-class UE4IK_API AnimNode_BipedHipAdjustment
+USTRUCT()
+struct UE4IK_API FAnimNode_BipedHipAdjustment : public FAnimNode_SkeletalControlBase
 {
+
+	GENERATED_USTRUCT_BODY()
+
 public:
-	AnimNode_BipedHipAdjustment();
-	~AnimNode_BipedHipAdjustment();
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Links)
+	FPoseLink InputPose;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = IK, meta = (PinShownByDefault))
+	FFabrikBipedLegChain LeftLeg;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = IK, meta = (PinShownByDefault))
+	FFabrikBipedLegChain RightLeg;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = IK, meta = (PinShownByDefault))
+	FIKBone PelvisBone;
+
+public:
+
+	FAnimNode_BipedHipAdjustment()
+		:
+		DeltaTime(0.0f),
+		bInitSuccess(false)
+	{ }
+
+	// FAnimNode_SkeletalControlBase Interface
+	virtual void UpdateInternal(const FAnimationUpdateContext& Context);
+	virtual void EvaluateComponentSpaceInternal(FComponentSpacePoseContext& Context);
+	virtual void EvaluateSkeletalControl_AnyThread(FComponentSpacePoseContext& Output, TArray<FBoneTransform>& OutBoneTransforms);
+	virtual bool IsValidToEvaluate(const USkeleton* Skeleton, const FBoneContainer& RequiredBones);
+	virtual void InitializeBoneReferences(const FBoneContainer& RequiredBones);
+	// End FAnimNode_SkeletalControlBase Interface
+
+private:
+	float DeltaTime;
+	bool bInitSuccess;
+
 };
