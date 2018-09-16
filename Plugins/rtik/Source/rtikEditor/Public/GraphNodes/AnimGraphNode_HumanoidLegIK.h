2b3d75e69f319093bcfb3e4494150afde9daab42
Morva Kristóf <kristof92@gmail.com>
2018-09-16 16:58:31 +0200

    Add missing GetNode functions

--- Plugins/rtik/Source/rtikEditor/Public/GraphNodes/AnimGraphNode_HumanoidLegIK.h
+++ Plugins/rtik/Source/rtikEditor/Public/GraphNodes/AnimGraphNode_HumanoidLegIK.h
@@ -16,6 +16,7 @@ public:
 	FText GetNodeTitle(ENodeTitleType::Type TitleType) const override;
 	FLinearColor GetNodeTitleColor() const override;
 	FString GetNodeCategory() const override;
+	virtual const FAnimNode_SkeletalControlBase* GetNode() const override { return &Node; }
 
 protected:
 	virtual FText GetControllerDescription() const;
