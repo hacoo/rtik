113b5b81995942981beced218ba4d27a162a65a9
Alexander <skohliks1@gmail.com>
2020-07-22 17:41:00 -0400

    Fixed Shipping building and compilation for 4.25.1

--- Plugins/rtik/Source/rtik/rtik.cpp
+++ Plugins/rtik/Source/rtik/rtik.cpp
@@ -3,7 +3,8 @@
 #include "rtik.h"
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, rtik, "rtik" );
+//IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, rtik, "rtik" );
+IMPLEMENT_MODULE( FDefaultGameModuleImpl, rtik );
 
 DEFINE_LOG_CATEGORY(LogRTIK)
 
